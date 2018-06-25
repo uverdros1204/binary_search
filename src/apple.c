11e74d82c0027f9f0d7c2e57f194b7e42725d28b
uverdros1204 <yamamuro.h.ab@m.titech.ac.jp>
2018-06-25 19:36:44 +0900

    おあ

--- src/apple.c
+++ src/apple.c
@@ -4,14 +4,36 @@ int n;
 int k;
 int A[100000];
 
-
+    int p(int m) {
+        int i,z;
+        z = 0;
+        for(i = 0; i < n; i++){
+        if(z > k)
+        {return 0;}
+        else {
+            if(A[i] % m == 0) {z = z +(A[i] / m);}
+            else {z = z + 1 + (A[i] / m);}}}
+            if (z <= k) return 1;
+        else return 0;}
 int main(){
   int i, lb, ub;
   scanf("%d%d", &n, &k);
-  for(i = 0; i < n; i++){
+
+      for(i = 0; i < n; i++){
     scanf("%d", &A[i]);
   }
-
-
+    lb = 0;
+    ub = A[n-1] ;
+    while (ub - lb > 1){
+        unsigned int m = (lb + ub) / 2;
+        if (p(m)){
+            ub = m;
+            
+        }
+        else {
+        lb = m;
+              }
+}
+printf("%d\n" , ub);
   return 0;
 }
--- src/spear.c
+++ src/spear.c
@@ -4,14 +4,30 @@ int n;
 int k;
 int A[100000];
 
+int p(int m) {
+    for (i = 0;i < n;i++)
+        
+}
+
+
 
 int main(){
   int i, lb, ub;
   scanf("%d%d", &n, &k);
+    lb = ;
+    ub = ;
   for(i = 0; i < n; i++){
     scanf("%d", &A[i]);
   }
-
-
+    while (ub - lb > 1){
+        unsigned int m = (lb + ub) / 2;
+        if (p(m)){
+            ub = m;
+        }
+        else {
+            lb = m;
+        }
+    }
+    printf("%d\n" , ub);
   return 0;
 }
