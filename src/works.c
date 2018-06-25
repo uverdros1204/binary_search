19798cbb1a6d0db01f29e11166d74147ed20f5c4
山室宏貴 <yamamurohiroshitaka@yamamurohiroshitakanoMacBook-Air.local>
2018-06-26 08:15:11 +0900

    終わった

--- src/works.c
+++ src/works.c
@@ -4,6 +4,19 @@ int n;
 int k;
 int A[100000];
 
+int p(int m){ 
+   int i,t,z
+   t = 0;
+   z = 1;
+   for(i = 0; i < n; i++){
+   if(A[i] > m){return 0;}
+else {
+      if ( t + A[i] > m){
+          t + A[i] = A[i];
+          z = z + 1;}}}
+
+  if (z <= k) return 1;
+else return 0;}
 
 int main(){
   int i, lb, ub;
@@ -11,7 +24,18 @@ int main(){
   for(i = 0; i < n; i++){
     scanf("%d", &A[i]);
   }
-
-
+lb = 0;
+ub = ;
+while (ub - lb > 1){
+        unsigned int m = (lb + ub) / 2;
+        if (p(m)){
+            ub = m;
+            
+        }
+        else {
+        lb = m;
+              }
+}
+printf("%d\n" , ub);
   return 0;
 }
