b110cc8e539cdb3b992017d09671c8af24a58579
uverdros1204 <yamamuro.h.ab@m.titech.ac.jp>
2018-06-25 20:07:12 +0900

    終わった

--- src/spear.c
+++ src/spear.c
@@ -5,20 +5,22 @@ int k;
 int A[100000];
 
 int p(int m) {
-    for (i = 0;i < n;i++)
-        
-}
+    int i,z;
+    z = 0;
+    for(i = 0; i < n; i++){
+    z = z +(A[i]/ m);}
+    return z < k;}
 
 
 
 int main(){
   int i, lb, ub;
   scanf("%d%d", &n, &k);
-    lb = ;
-    ub = ;
+   
   for(i = 0; i < n; i++){
     scanf("%d", &A[i]);
-  }
+      lb = 0;
+      ub = A[n-1]; }
     while (ub - lb > 1){
         unsigned int m = (lb + ub) / 2;
         if (p(m)){
@@ -28,6 +30,6 @@ int main(){
             lb = m;
         }
     }
-    printf("%d\n" , ub);
+    printf("%d\n" , lb);
   return 0;
 }
